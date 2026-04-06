1e205ce31b4e800133fdef244996ae6edcfe486b
egfedoseev <eg.fedoseev2014@yandex.ru>
2026-04-06 21:57:56 +0300

    done 1

--- java-solutions/info/kgeorgiy/ja/fedoseev/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/fedoseev/walk/Walk.java
@@ -3,29 +3,63 @@ package info.kgeorgiy.ja.fedoseev.walk;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Optional;
 
 public class Walk {
-    private static int hashFile(Path path) {
-        int hash = 0;
-        try (BufferedInputStream fileInputStream = new BufferedInputStream(Files.newInputStream(path))) {
-            hash = FNWHasher.FNV1Hash(fileInputStream);
-        } catch (FileNotFoundException e) {
-            System.err.println("File " + path + " not found: " + e.getMessage());
-        } catch (IOException e) {
-            hash = 0;
-        }
-        return hash;
+    private static int hashFile(Optional<Path> path) {
+        return path.map(Walk::hashFile).orElse(0);
     }
 
-    static void main(String[] args) {
-        if (args.length != 2) {
+    static int hashFile(Path path) {
+        try (BufferedInputStream fileInputStream = new BufferedInputStream(Files.newInputStream(path))) {
+            return FNWHasher.FNV1Hash(fileInputStream);
+        } catch (Throwable e) {
+            return 0;
+        }
+    }
+
+    static Optional<Path> tryParsePath(String pathString) {
+        if (pathString == null) {
+            return Optional.empty();
+        }
+        try {
+            return Optional.of(Path.of(pathString));
+        } catch (InvalidPathException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args == null || args.length != 2) {
             System.err.println("Usage: INPUT OUTPUT");
             return;
         }
-        Path inputPath = Path.of(args[0]);
-        Path outputPath = Path.of(args[1]);
+        Optional<Path> parsedInput = tryParsePath(args[0]);
+        if (parsedInput.isEmpty()) {
+            System.err.println("Invalid input path: " + args[0]);
+            return;
+        }
+        Path inputPath = parsedInput.get();
+
+        Optional<Path> parsedOutput = tryParsePath(args[1]);
+        if (parsedOutput.isEmpty()) {
+            System.err.println("Invalid output path: " + args[1]);
+            return;
+        }
+        Path outputPath = parsedOutput.get();
+
+        try {
+            final Path parent = outputPath.getParent();
+            if (parent != null) {
+                Files.createDirectories(parent);
+            }
+        } catch (IOException e) {
+            System.err.println("Can't create parent directories for output file: " + e.getMessage());
+            return;
+        }
 
         try (BufferedReader inputReader = Files.newBufferedReader(inputPath, StandardCharsets.UTF_8)) {
             try (BufferedWriter outputWriter = Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8, StandardOpenOption.CREATE)) {
@@ -44,13 +78,18 @@ public class Walk {
         String pathString = inputReader.readLine();
         while (pathString != null) {
             try {
-                outputWriter.write(String.format("%08x %s", hashFile(Path.of(pathString)), pathString));
-                outputWriter.newLine();
+                processRegularFile(outputWriter, tryParsePath(pathString), pathString);
             } catch (IOException e) {
                 System.err.println("Can't write to output file: " + e.getMessage());
                 return;
             }
             pathString = inputReader.readLine();
         }
+    }
+
+    static void processRegularFile(BufferedWriter outputWriter, Optional<Path> path, String pathString) throws IOException {
+        int hash = hashFile(path);
+        outputWriter.write(String.format("%08x %s", hash, pathString));
+        outputWriter.newLine();
     }
 }
